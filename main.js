60fd4569d0e00cff48f0fc3d69ea8c42180b39c3
Indiorlei <indiorlei.oliveira@cwi.com.br>
2024-04-15 14:47:08 -0300

    feat: :sparkles: initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const qr = require("qrcode");
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/qrcode", (req, res) => {
+  const dataToEncode = req.query.data || "Hello, world!";
+
+  qr.toDataURL(dataToEncode, (err, url) => {
+    if (err) {
+      res.status(500).send("Erro ao criar o QR code.");
+    } else {
+      res.send(`<img src="${url}" alt="QR Code">`);
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado em http://localhost:${PORT}`);
+});
